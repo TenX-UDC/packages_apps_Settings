2034ae0cad7b396806663decb56ca38fa58db695
DarkJoker360 <simoespo159@gmail.com>
2023-03-31 10:42:42 +0000

    Settings: Add a toggle to force LTE_CA
    Change-Id: Icf913191ff38cd7db4dc17ffde9410dfcc38a1c0
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -333,6 +333,7 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings impleme
                 subscriptionPreferenceControllers) {
             controller.init(mSubId);
         }
+        use(ForceLteCaPreferenceController.class).init(mSubId);
     }
 
     @Override
